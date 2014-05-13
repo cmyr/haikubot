c6a0890e66dc6b85a863b32d26ada95c8d9e7fa4
cmyr <colin@cmyr.net>
2014-05-13 13:44:29 -0400

    added haiku_writer class, which basically works

--- haikuwriter.py
+++ haikuwriter.py
@@ -3,25 +3,51 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import poetryutils2
-
+import twitterstream
 
 def generate_haiku():
-    pass
+    haikuer = poetryutils2.Haikuer(debug=True)
 
-def haiku_test():
-    # lines = poetryutils2.utils.lines_from_file('/Users/cmyr/Documents/twitterpoems/20ktst.txt')
+
+    filters = []
+    filters.append(poetryutils2.filters.numeral_filter)
+    filters.append(poetryutils2.filters.url_filter)
+    filters.append(poetryutils2.filters.ascii_filter)
+    filters.append(poetryutils2.filters.low_letter_filter(0.9))
+    filters.append(poetryutils2.filters.swears_filter())
+    filters.append(poetryutils2.filters.real_word_ratio_filter(0.9))
+    filters.append(poetryutils2.filters.syllable_count_filter('5,7'))
+    filters.append(poetryutils2.filters.blacklist_filter([
+        'oomf'
+        ]))
+
+    source = poetryutils2.line_iter(twitter_stream_source(), filters)
+    for beauty in haikuer.generate_from_source(source):
+        poem = '%s\n%s\n%s\n\n' % (beauty[0], beauty[1], beauty[2])
+        print(poem.encode('utf8'))
+
+
+def debug_source():
     lines = poetryutils2.utils.lines_from_file('/Users/cmyr/tweetdbm/may09.txt')
-    haik = poetryutils2.Haikuer()
-
     filters = [poetryutils2.filters.url_filter,
         poetryutils2.filters.ascii_filter,
         poetryutils2.filters.low_letter_filter(0.9)]
-
     source = poetryutils2.line_iter(lines, filters)
 
-    for beauty in haik.generate_from_source(source):
-        poem = '%s\n%s\n%s\n\n' % (beauty[0], beauty[1], beauty[2])
-        print(poem.encode('utf8'))
+def twitter_stream_source():
+    streamer = twitterstream.TwitterStream()
+    stream = streamer.stream_iter(
+        languages=['en'],
+        user_agent="@haiku9000",
+        just_text=True
+        )
+    return stream
+
+def haiku_test():
+    generate_haiku()
+    # lines = poetryutils2.utils.lines_from_file('/Users/cmyr/Documents/twitterpoems/20ktst.txt')
+    
+
 
 
 
